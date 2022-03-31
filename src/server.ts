8df5b518dc2a5e2e3252cf58c65bc6c58ce299a3
Tiago Rodrigues <tiagods127@gmail.com>
2022-03-30 23:14:16 -0300

    feat: create server

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+
+import { routes } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT, () => console.log("Server is running"));
