a69d5930fac76fe072fc6ba53574c29eafb86fde
Tiago Rodrigues <tiagods127@gmail.com>
2022-04-01 09:47:47 -0300

    refactor: add dev port

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT, () => console.log("Server is running"));
+app.listen(process.env.PORT || 3000, () => console.log("Server is running"));
