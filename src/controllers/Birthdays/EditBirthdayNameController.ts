e88478393ce449e439bb411548a8cdbedf7944a1
Tiago Rodrigues <tiagods127@gmail.com>
2022-03-31 21:59:58 -0300

    feat: add EditBirthdayNameController

---
+++ src/controllers/Birthdays/EditBirthdayNameController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { EditBirthdayNameService } from "../services/Birthdays/EditBirthdayNameService";
+import { Birthday } from "../types/Birthday";
+
+export class EditBirthdayNameController {
+	async handle(req: Request, res: Response) {
+		const data = <Birthday>req.body;
+
+		try {
+			const service = new EditBirthdayNameService();
+			const result = await service.execute(data);
+
+			if (result instanceof Error) {
+				return res
+					.status(400)
+					.json({ succes: false, error: result.message });
+			}
+
+			res.status(200).json({ succes: true });
+		} catch (e) {
+			return res.status(500).json({ succes: false, error: e });
+		}
+	}
+}
