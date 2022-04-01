bd4d2b1fd7a828216e6ea338274bbd958bd48656
Tiago Rodrigues <tiagods127@gmail.com>
2022-03-31 21:56:21 -0300

    feat: add new routes (get and put)

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
-import { Request, Response, Router } from "express";
-import { CreateBirthdayController } from "./controllers/CreateBirthdayController";
+import { Router } from "express";
+
+import { CreateBirthdayController } from "./controllers/Birthdays/CreateBirthdayController";
+import { ListAllBirthdaysController } from "./controllers/Birthdays/ListAllBirthdaysController";
+import { EditBirthdayNameController } from "./controllers/Birthdays/EditBirthdayNameController";
 
 const routes = Router();
 
 routes.post("/birthdays", new CreateBirthdayController().handle);
+routes.get("/birthdays", new ListAllBirthdaysController().handle);
+routes.put("/birthdays/edit-name", new EditBirthdayNameController().handle);
 
 export { routes };
