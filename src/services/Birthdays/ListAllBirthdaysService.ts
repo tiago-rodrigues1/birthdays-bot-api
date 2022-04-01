57cd27b268dfaacb9b50bbde4269812fe4ed7f39
Tiago Rodrigues <tiagods127@gmail.com>
2022-03-31 21:59:05 -0300

    feat: add ListAllBirthdaysService

---
+++ src/services/Birthdays/ListAllBirthdaysService.ts
@@ -0,0 +1,27 @@
+import { connectToDb } from "../MongoDb";
+
+export class ListAllBirthdaysService {
+	async execute() {
+		try {
+			const connection = await connectToDb();
+			const db = connection?.db;
+
+			let results;
+
+			if (db) {
+				const cursor = db.collection("birthdays").find();
+				results = await cursor.toArray();
+
+				if (!results) {
+					return new Error(
+						"Não foi possível listar os aniversariantes"
+					);
+				}
+			}
+
+			return results;
+		} catch (e) {
+			return new Error(`${e}`);
+		}
+	}
+}
