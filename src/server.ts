e9b35914c1e1cb5ed4c6b6e25db27277cfe430f1
Tiago Rodrigues <tiagods127@gmail.com>
2022-04-05 16:37:12 -0300

    refactor: remove function to send messages

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import { MessageEmbed, WebhookMessageOptions } from "discord.js";
-
 import { routes } from "./routes";
-
-import { discordMessager } from "./services/Discord";
-import { ListBirthdaysByDateService } from "./services/Birthdays/ListBirthdaysByDateService";
 
 const app = express();
 
@@ -14,74 +9,4 @@ app.use(routes);
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server is running");
-
-	const oneMinuteMilliseconds = 1000 * 60;
-	const tenMinutesMilliseconds = oneMinuteMilliseconds * 10;
-
-	setInterval(async () => {
-		const currentHour = new Date().getHours();
-
-		if (currentHour === 8) {
-			try {
-				const result = await sendHappyBirthdays();
-
-				if (!result) {
-					throw new Error();
-				}
-			} catch (e) {
-				console.log(e);
-			}
-		}
-	}, tenMinutesMilliseconds);
 });
-
-async function sendHappyBirthdays() {
-	const todayDate = new Date().toLocaleDateString("pt-br");
-	const [currentDay, currentMonth] = todayDate.split("/");
-
-	const queryDate = `${currentDay}/${currentMonth}}`;
-
-	let birthdaysMessage: string = "";
-
-	try {
-		const service = new ListBirthdaysByDateService();
-		const dbResponse = (await service.execute(queryDate)) || [];
-
-		if (dbResponse instanceof Error) {
-			console.log(dbResponse.message);
-		} else {
-			dbResponse.forEach((birthday) => {
-				birthdaysMessage += `- ${birthday.name}\n`;
-			});
-		}
-	} catch (e) {
-		console.log(
-			"Não foi possível resgatar os aniversariantes no banco de dados:\n",
-			e
-		);
-	}
-
-	let discordResponse;
-
-	try {
-		const embed = new MessageEmbed()
-			.setColor("#B6FFCE")
-			.setTitle("Feliz aniversário para")
-			.setDescription(`${birthdaysMessage}`);
-
-		const discordMessagerOptions: WebhookMessageOptions = {
-			content: birthdaysMessage ? null : "Não temos aniversariantes hoje",
-			embeds: birthdaysMessage ? [embed] : undefined,
-		};
-
-		discordResponse = await discordMessager(discordMessagerOptions);
-	} catch (e) {
-		console.log("Não foi possível enviar a mensagem: \n", e);
-	}
-
-	if (discordResponse) {
-		return true;
-	} else {
-		return false;
-	}
-}
